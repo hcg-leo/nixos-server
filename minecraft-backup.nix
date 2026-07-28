531c90aab05c94c6ee7484cf2b9b0c68778b10b1
Aran Thananjayan <aran20111118@gmail.com>
2026-07-29 00:53:18 +0100

    Refactor Minecraft backup service configuration

--- minecraft-backup.nix
+++ minecraft-backup.nix
@@ -13,17 +13,14 @@
       Type = "oneshot";
       User = "root";
       
-      # 1. Stop the Podman Minecraft server before starting the backup
       ExecStartPre = "${pkgs.systemd}/bin/systemctl stop podman-minecraft-server.service";
       
-      # 2. Run the backup (using 'sync' to match source to destination exactly)
       ExecStart = ''
         ${pkgs.rclone}/bin/rclone sync /var/lib/minecraft gdrive:MinecraftServerBackup \
           --config /root/secrets/rclone.conf \
           --verbose
       '';
 
-      # 3. Start the Podman Minecraft server again after the backup completes (or if it fails)
       ExecStopPost = "${pkgs.systemd}/bin/systemctl start podman-minecraft-server.service";
     };
   };
