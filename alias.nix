2a8f76594fe0b1228a9f488956a22b9739a26f71
Aran Thananjayan <aran20111118@gmail.com>
2026-08-01 23:06:57 +0100

    Refactor alias names for clarity and consistency

--- alias.nix
+++ alias.nix
@@ -6,22 +6,24 @@
     rebuild = "sudo nixos-rebuild switch";
     
 
-    # config files
+    # config
     networking-config = "sudo nvim ~/nixos-server/networking.nix";
     alias-config = "sudo nvim ~/nixos-server/alias.nix";
     config = "sudo nvim ~/nixos-server/configuration.nix";
     
+    # minecraft config
     minecraft-config = "sudo nvim ~/nixos-server/minecraft.nix";
-    minecraft-backup_config = "sudo nvim ~/nixos-server/minecraft-backup.nix";
-
-    media-config = "sudo nvim ~/nixos-server/jellyfin.nix";
-
-    # minecraft
+    minecraft-backup-config = "sudo nvim ~/nixos-server/minecraft-backup.nix";
+    
+    # minecraft server
     minecraft-console = "sudo podman exec -it minecraft-server rcon-cli";
     minecraft-console_logs = "sudo journalctl -fu podman-minecraft-server.service";
     minecraft-backup = "sudo systemctl start --no-block minecraft-backup.service";
-    minecraft-backup_logs = "sudo journalctl -u minecraft-backup.service -f";
+    minecraft-backup-logs = "sudo journalctl -u minecraft-backup.service -f";
     minecraft-stop = "sudo podman stop -t 60 minecraft-server";
+
+    # media config
+    media-config = "sudo nvim ~/nixos-server/jellyfin.nix";
     
     # other
     reboot = "sudo reboot";
