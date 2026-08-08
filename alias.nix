0c5653159f1c037648ab6401347b6d9db69f616b
Aran Thananjayan <aran20111118@gmail.com>
2026-08-08 21:17:08 +0100

    Update alias.nix

--- alias.nix
+++ alias.nix
@@ -11,22 +11,22 @@
     alias-config = "sudo nvim ~/nixos-server/alias.nix";
     config = "sudo nvim ~/nixos-server/configuration.nix";
     
-    # minecraft config
+    # minecraft
     minecraft-config = "sudo nvim ~/nixos-server/minecraft.nix";
     minecraft-backup-config = "sudo nvim ~/nixos-server/minecraft-backup.nix";
     
-    # minecraft server
     minecraft-console = "sudo podman exec -it minecraft-server rcon-cli";
     minecraft-console_logs = "sudo journalctl -fu podman-minecraft-server.service";
     minecraft-backup = "sudo systemctl start --no-block minecraft-backup.service";
     minecraft-backup-logs = "sudo journalctl -u minecraft-backup.service -f";
     minecraft-stop = "sudo podman stop -t 60 minecraft-server";
 
-    # media config
+    # media
     media-config = "sudo nvim ~/nixos-server/media.nix";
 
     # vpn torrent
     mullvad = "sudo ip netns exec qbtns curl -s ifconfig.me";
+    vpn-torrent = "sudo nvim ~/nixos-server/vpn-torrent.nix";
     
     # other
     reboot = "sudo reboot";
