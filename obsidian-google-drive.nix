770c11b9e84c5f7fc27d1e93b604bae60dbb74a5
hcg-leo <aran20111118@gmail.com>
2026-09-20 11:21:05 +0100

    obsidian sync via google drive api

--- obsidian-google-drive.nix
+++ obsidian-google-drive.nix
@@ -1,47 +1,27 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.podman = {
-    enable = true;
-    dockerCompat = true;
-  };
+  # secret file: /root/secrets/ogd-server.env
+  #   CLIENT_SECRET=GOCSPX-yourclientsecret
+
+  virtualisation.podman.enable = true;
 
   virtualisation.oci-containers = {
     backend = "podman";
     containers.ogd-server = {
       image = "docker.io/richardxregistry/ogd-server:latest";
       autoStart = true;
-
-      # bound to loopback only - nginx is the only thing that talks to it
       ports = [ "127.0.0.1:3005:3005" ];
-
       environmentFiles = [ "/root/secrets/ogd-server.env" ];
     };
   };
 
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = "CHANGE-ME@example.com";
-  };
-
-  services.nginx = {
+  services.caddy = {
     enable = true;
-    recommendedProxySettings = true;
-    recommendedTlsSettings = true;
-
-    virtualHosts."hcg-leo.duckdns.org" = {
-      enableACME = true;
-      forceSSL = true;
-
-      locations."/api/access" = {
-        proxyPass = "http://127.0.0.1:3005";
-      };
-
-      # everything else gets nothing
-      locations."/" = {
-        return = "404";
-      };
-    };
+    email = "CHANGE-ME@example.com";
+    virtualHosts."hcg-leo.duckdns.org".extraConfig = ''
+      reverse_proxy 127.0.0.1:3005
+    '';
   };
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
